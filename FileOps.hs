e034547a9b22cc8cb24a32998495c0b622985345
Rebecca Skinner <rebecca@rebeccaskinner.net>
2011-08-27 22:57:41 -0500

    Moved curses test code into pager, and added pagifying code to FileOps

--- FileOps.hs
+++ FileOps.hs
@@ -2,5 +2,15 @@ module FileOps where
 import Data.List.Split
 import Utils
 
+-- Break the string 's' into lines that are at most 'n' characters wide, then
+-- return a list of lines
 hardWrapLines n s = concatMap (chunk n) $ wordsBy (=='\n') s
+
+-- Get a version of string 's' wrapped to 'n' characters using '\n' as the
+-- newline character
 hardWrap n s = joinByElem '\n' $ hardWrapLines n s
+
+-- Get a page 'n' from the text, using a terminal that is 'x' characters
+-- wide by 'y' characters high from the string 's'.
+getPage x y n s =
+    take y $ drop (min (n*(y-1)) ((length s) - y)) $ hardWrapLines x s
--- Pager.hs
+++ Pager.hs
@@ -1,3 +1,36 @@
-module Pager where
+-- module Pager where
+import System (getArgs)
+import Graphics.Vty
+import Data.Word
+import System.IO
+import FileOps
 
+fi = fromIntegral
 
+main = do
+    vt <- mkVty
+    fstr <- (getArgs >>= (return . head) >>= readFile)
+    DisplayRegion w h <- display_bounds $ terminal vt
+    putStrLn $ show $ DisplayRegion w h
+    showInPager vt 0 w h fstr
+
+showInPager vt y sx sy fstr = do
+    let stopPlay      = shutdown vt >> return ()
+        pageIncrement = if [] == stxt then stopPlay else showInPager vt (y + 1) sx sy fstr
+        pageDecrement = showInPager vt (y - 1) sx sy fstr
+        stxt          = getPage (fi sx) (fi sy) y fstr
+
+    update vt (current_pic stxt)
+    k <- next_event vt
+    case k of EvKey (KASCII ' ') [] -> pageIncrement
+              EvKey KDown        [] -> pageIncrement
+              EvKey KUp          [] -> pageDecrement
+              EvKey KEsc         [] -> stopPlay
+              EvKey (KASCII 'q') [] -> stopPlay
+              EvResize nx ny        -> showInPager vt (min y (toEnum ny - 2))
+                                               (toEnum nx)
+                                               (toEnum ny)
+                                               fstr
+              _                     -> showInPager vt y sx sy fstr
+
+current_pic lines = pic_for_image $ vert_cat $ map (string def_attr) lines
